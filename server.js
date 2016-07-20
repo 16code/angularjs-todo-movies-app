dce4df83db16c63cac2d42d3427e7e4fb605f1b1
Liuxin <130866602@qq.com>
2016-07-20 23:27:21 +0800

    update

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 const app = express();
 app.use(express.static(`${__dirname}/dist`));
 app.get('*', (req, res) => {
- res.sendFile(path.join(`${__dirname}/dist/index.html`));
+    res.sendFile(path.join(`${__dirname}/dist/index.html`));
 });
-app.listen(8080, () => {
- console.log('App listening on port 8080!');
+app.listen(80, () => {
+    console.log('App listening on port 80!');
 });
 module.exports = app;
