189f753d31fd5e523d37f9b58eb6414e454d6d3a
16code <130866602@qq.com>
2016-08-02 17:03:13 +0800

    update pagination

--- src/components/pagination/pagination.controller.js
+++ src/components/pagination/pagination.controller.js
@@ -12,67 +12,67 @@ class PaginationController {
         this.config.pagesLength = Number(this.config.pagesLength) || 9;
         this.$onChanges = (changesObj) => {
             if (changesObj.config && changesObj.config.currentValue) {
-                getPagination();
+                this.getPagination();
             }
         };
         if (this.config.pagesLength % 2 === 0) {
             // 如果不是奇数的时候处理一下
             this.config.pagesLength = this.config.pagesLength - 1;
         }
-        const self = this;
+    }
+    getPagination() {
         /* eslint complexity: ["error", 14]*/
-        function getPagination() {
-            // conf.currentPage
-            self.config.currentPage = parseInt(self.config.currentPage, 10) || 1;
-            // conf.totalItems
-            self.config.totalItems = parseInt(self.config.totalItems, 10) || 0;
-            // conf.itemsPerPage (default:15)
-            self.config.itemsPerPage = parseInt(self.config.itemsPerPage, 10) || 20;
-            // numberOfPages
-            self.config.numberOfPages = Math.ceil(self.config.totalItems / self.config.itemsPerPage);
-            // 如果分页总数>0，并且当前页大于分页总数
-            if (self.config.numberOfPages > 0 && (self.config.currentPage > self.config.numberOfPages)) {
-                self.config.currentPage = self.config.numberOfPages;
-            }
-            self.pageList = [];
+        const self = this;
+        // conf.currentPage
+        self.config.currentPage = parseInt(self.config.currentPage, 10) || 1;
+        // conf.totalItems
+        self.config.totalItems = parseInt(self.config.totalItems, 10) || 0;
+        // conf.itemsPerPage (default:15)
+        self.config.itemsPerPage = parseInt(self.config.itemsPerPage, 10) || 20;
+        // numberOfPages
+        self.config.numberOfPages = Math.ceil(self.config.totalItems / self.config.itemsPerPage);
+        // 如果分页总数>0，并且当前页大于分页总数
+        if (self.config.numberOfPages > 0 && (self.config.currentPage > self.config.numberOfPages)) {
+            self.config.currentPage = self.config.numberOfPages;
+        }
+        self.pageList = [];
 
-            if (self.config.numberOfPages <= self.config.pagesLength) {
-                // 判断总页数如果小于等于分页的长度，若小于则直接显示
-                for (let i = 1; i <= self.config.numberOfPages; i++) {
+        if (self.config.numberOfPages <= self.config.pagesLength) {
+            // 判断总页数如果小于等于分页的长度，若小于则直接显示
+            for (let i = 1; i <= self.config.numberOfPages; i++) {
+                self.pageList.push(self.parseStateToUri(i));
+            }
+        } else {
+            // 总页数大于分页长度（此时分为三种情况：1.左边没有...2.右边没有...3.左右都有...）
+            // 计算中心偏移量
+            const offset = (self.config.pagesLength - 1) / 2;
+            if (self.config.currentPage <= offset) {
+                // 左边没有...
+                for (let i = 1; i <= offset + 1; i++) {
                     self.pageList.push(self.parseStateToUri(i));
                 }
-            } else {
-                // 总页数大于分页长度（此时分为三种情况：1.左边没有...2.右边没有...3.左右都有...）
-                // 计算中心偏移量
-                const offset = (self.config.pagesLength - 1) / 2;
-                if (self.config.currentPage <= offset) {
-                    // 左边没有...
-                    for (let i = 1; i <= offset + 1; i++) {
-                        self.pageList.push(self.parseStateToUri(i));
-                    }
-                    self.pageList.push({page: '...'});
-                    self.pageList.push(self.parseStateToUri(self.config.numberOfPages));
-                } else if (self.config.currentPage > self.config.numberOfPages - offset) {
-                    self.pageList.push(self.parseStateToUri(1));
-                    self.pageList.push({page: '...'});
-                    for (let i = offset + 1; i >= 1; i--) {
-                        self.pageList.push(self.parseStateToUri(self.config.numberOfPages - i));
-                    }
-                    self.pageList.push(self.parseStateToUri(self.config.numberOfPages));
-                } else {
-                    // 最后一种情况，两边都有...
-                    self.pageList.push(self.parseStateToUri(1));
-                    self.pageList.push({page: '...'});
-                    for (let i = Math.ceil(offset / 2); i >= 1; i--) {
-                        self.pageList.push(self.parseStateToUri(self.config.currentPage - i));
-                    }
-                    self.pageList.push(self.parseStateToUri(self.config.currentPage));
-                    for (let i = 1; i <= offset / 2; i++) {
-                        self.pageList.push(self.parseStateToUri(self.config.currentPage + i));
-                    }
-                    self.pageList.push({page: '...'});
-                    self.pageList.push(self.parseStateToUri(self.config.numberOfPages));
+                self.pageList.push({page: '...'});
+                self.pageList.push(self.parseStateToUri(self.config.numberOfPages));
+            } else if (self.config.currentPage > self.config.numberOfPages - offset) {
+                self.pageList.push(self.parseStateToUri(1));
+                self.pageList.push({page: '...'});
+                for (let i = offset + 1; i >= 1; i--) {
+                    self.pageList.push(self.parseStateToUri(self.config.numberOfPages - i));
                 }
+                self.pageList.push(self.parseStateToUri(self.config.numberOfPages));
+            } else {
+                // 最后一种情况，两边都有...
+                self.pageList.push(self.parseStateToUri(1));
+                self.pageList.push({page: '...'});
+                for (let i = Math.ceil(offset / 2); i >= 1; i--) {
+                    self.pageList.push(self.parseStateToUri(self.config.currentPage - i));
+                }
+                self.pageList.push(self.parseStateToUri(self.config.currentPage));
+                for (let i = 1; i <= offset / 2; i++) {
+                    self.pageList.push(self.parseStateToUri(self.config.currentPage + i));
+                }
+                self.pageList.push({page: '...'});
+                self.pageList.push(self.parseStateToUri(self.config.numberOfPages));
             }
         }
     }
